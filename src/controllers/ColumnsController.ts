abcd1387d080db0b2793ed2a262753fd6fcfcbb5
Cléberson José de Andrade <clebersonjosea@gmail.com>
2020-09-16 09:03:38 -0300

    Unindo Boards Controller com Columns Controller

--- src/controllers/ColumnsController.ts
+++ src/controllers/ColumnsController.ts
@@ -2,8 +2,46 @@ import { Request, Response } from "express";
 import db from "../database/connection";
 
 export default class ColumnsController {
-  async index() {}
-  async createTasks() {}
+  async index(request: Request, response: Response) {
+    const columns = await db("columns")
+      .whereExists(function () {
+        this.select("columns.*")
+          .from("columns")
+          .whereRaw("`columns`.`columnCode`=`columnCode`")
+          .whereRaw("`columns`.`name`=`name`")
+          .whereRaw("`columns`.`position`=`position`");
+      })
+      .then((data) => {
+        return response.status(200).json(data);
+      })
+      .catch(() => {
+        return response.status(400).json({
+          error: "Unexpected error while getting the columns",
+        });
+      });
+    return columns;
+  }
+
+  async createColumn(request: Request, response: Response) {
+    const { name, position } = request.body;
+    const trx = await db.transaction();
+
+    try {
+      await trx("columns").insert({
+        name,
+        position,
+      });
+
+      await trx.commit();
+      return response.status(201).send();
+    } catch (err) {
+      await trx.rollback();
+
+      return response.status(400).json({
+        error: "Unexpected error while creating new column",
+      });
+    }
+  }
 
   async editColumn(request: Request, response: Response) {
     const { columnCode, name, position } = request.body;
--- src/routes.ts
+++ src/routes.ts
@@ -1,17 +1,19 @@
 import express from "express";
-import BoardsController from "./controllers/BoardsController";
 import ColumnsController from "./controllers/ColumnsController";
+import TasksController from "./controllers/TasksController";
 
 const routes = express.Router();
-const boardsController = new BoardsController();
 const columnsController = new ColumnsController();
-
-routes.get("/boards", boardsController.index);
-routes.post("/boards", boardsController.createColumn);
+const tasksController = new TasksController();
 
 routes.get("/columns", columnsController.index);
-routes.post("/columns", columnsController.createTasks);
+routes.post("/columns", columnsController.createColumn);
 routes.put("/columns", columnsController.editColumn);
 routes.delete("/columns", columnsController.deleteColumn);
+
+routes.get("/tasks", tasksController.index);
+routes.post("/tasks", tasksController.createTask);
+routes.put("/tasks", tasksController.editTask);
+routes.delete("/tasks", tasksController.deleteTask);
 
 export default routes;
