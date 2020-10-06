5ba457ee5bc7fc38e2538f489aaa859f3114d562
Cléberson José de Andrade <clebersonjosea@gmail.com>
2020-10-06 10:38:53 -0300

    Test: Edit column

--- __tests__/unit/ColumnsController.test.ts
+++ __tests__/unit/ColumnsController.test.ts
@@ -1,27 +1,52 @@
-import app from '../../src/server'; // Link to your server file
+import app from '../../src/server';
 import supertest from 'supertest';
 import db from '../../src/database/connection';
 
 const request = supertest(app);
 
 describe("Tests from columns's controller", () => {
-  afterEach(async (done) => {
-    await db('columns').whereNot('columnCode', null).del();
-    done();
+  afterEach(async () => {
+    await db('columns').truncate();
   });
 
   it('Get columns', async (done) => {
     const response = await request.get('/columns');
+
     expect(response.status).toBe(200);
     done();
   });
 
   it('Create column', async (done) => {
-    const response = await request.post('/columns').send({
+    const create = await request.post('/columns').send({
       name: 'test',
       position: 1,
     });
-    expect(response.status).toBe(201);
+
+    const getColumn = await request.get('/columns');
+
+    const response = await JSON.parse(getColumn.text);
+
+    expect(response[0].name).toBe('test');
+    done();
+  });
+
+  it('Edit column', async (done) => {
+    const create = await request.post('/columns').send({
+      name: 'test',
+      position: 1,
+    });
+
+    const edit = await request.put('/columns').send({
+      columnCode: 1,
+      name: 'Otest',
+      position: 1,
+    });
+
+    const getColumn = await request.get('/columns');
+
+    const response = await JSON.parse(getColumn.text);
+    
+    expect(response[0].name).toBe('Otest');
     done();
   });
 });
