679d9cc31ff1d4f1cf205d30ff4b4ba33696dc8c
Cléberson José de Andrade <clebersonjosea@gmail.com>
2020-10-19 20:41:59 -0300

    Data base update

--- src/database/migrations/02_create_columns.ts
+++ src/database/migrations/02_create_columns.ts
@@ -2,9 +2,9 @@ import Knex from 'knex';
 
 export async function up(knex: Knex) {
   return knex.schema.createTable('columns', (table) => {
-    table.increments('columnCode').primary();
+    table.increments('id').primary();
     table.string('name').notNullable();
-    table.float('position').notNullable();
+    table.integer('position').notNullable();
   });
 }
 
